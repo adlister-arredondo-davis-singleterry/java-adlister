a443f8b1716472fb10e3dfe11ee2607e2b580b82
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-27 14:46:51 -0500

    added rs.getInt on line 63

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -59,8 +59,8 @@ public class MySQLUsersDao implements Users {
             rs.getLong("id"),
             rs.getString("username"),
             rs.getString("email"),
-            rs.getString("password")
+            rs.getString("password"),
+            rs.getInt("is_admin")
         );
     }
-
 }
