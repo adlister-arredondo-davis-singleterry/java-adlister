d995254ac560778c8963641d8f1528b7dc66b55f
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-27 14:47:17 -0500

    added int isAdmin property and getter/setter

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -7,6 +7,7 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private int isAdmin;
 
     public User() {}
 
@@ -16,11 +17,12 @@ public class User {
         setPassword(password);
     }
 
-    public User(long id, String username, String email, String password) {
+    public User(long id, String username, String email, String password, int isAdmin) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 
     public long getId() {
@@ -53,5 +55,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = Password.hash(password);
+    }
+
+    public int getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(int isAdmin) {
+        this.isAdmin = isAdmin;
     }
 }
