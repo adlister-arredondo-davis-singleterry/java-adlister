fd25db971bb4d863286058dcf2bcd4f506c869d2
Laura Davis <laura.l.davis.210@gmail.com>
2020-07-27 12:32:30 -0500

    Add isAdmin to User class to match users table

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -7,20 +7,23 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private boolean isAdmin;
 
     public User() {}
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, boolean isAdmin) {
         this.username = username;
         this.email = email;
         setPassword(password);
+        this.isAdmin = isAdmin;
     }
 
-    public User(long id, String username, String email, String password) {
+    public User(long id, String username, String email, String password, boolean isAdmin) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 
     public long getId() {
@@ -54,4 +57,9 @@ public class User {
     public void setPassword(String password) {
         this.password = Password.hash(password);
     }
+
+    public boolean isAdmin() { return isAdmin; }
+
+    public void setAdmin(boolean admin) { isAdmin = admin; }
+
 }
